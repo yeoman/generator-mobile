410eaf2d2bfac3c284db29ed34313105d4e21698
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-11-01 16:10:22 +0100

    Add placeholder Apple touch icon & <link>

--- app/index.js
+++ app/index.js
@@ -345,6 +345,7 @@ AppGenerator.prototype.h5bp = function h5bp() {
   this.copy('404.html', 'app/404.html');
   this.copy('robots.txt', 'app/robots.txt');
   this.copy('htaccess', 'app/.htaccess');
+  this.copy('apple-touch-icon.png', 'app/apple-touch-icon.png');
 };
 
 AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
