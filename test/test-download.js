e949ac7d6650543d29a0f7632d1c51f94de23d4c
alex <alex@cloudware.it>
2014-11-04 16:05:22 +0000

    Unstub HTTP hooks after each downloader test

--- test/test-download.js
+++ test/test-download.js
@@ -7,8 +7,12 @@ var download = require('../app/download');
 
 describe('download module', function() {
 
+  afterEach(function () {
+    nock.cleanAll();
+  })
+
   it('fetches latest release version and creates downloader', function(done) {
-    nock('https://api.github.com')
+    var scope = nock('https://api.github.com')
       .get('/repos/google/web-starter-kit/releases')
       .reply(200, [
         {tag_name: 'v1'},
@@ -21,6 +25,10 @@ describe('download module', function() {
       assert.deepEqual(ver, {tag_name: 'v2.5.2'});
       assert.equal(url, download.WSK_ZIP_URL + 'v2.5.2.zip');
       assert(d);
+
+      if (!scope.isDone()) {
+        assert.fail('pending mocks: ' + scope.pendingMocks());
+      }
       done();
     });
   });
