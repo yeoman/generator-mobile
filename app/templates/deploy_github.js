2a69b349b3ffff14eddd0ef93a43a24349ff4423
alex <alex@cloudware.it>
2014-11-27 16:44:24 +0000

    Split gulp deploy to github into multiple steps

--- app/templates/deploy_github.js
+++ app/templates/deploy_github.js
@@ -2,22 +2,44 @@
 
 var fs = require('fs');
 var path = require('path');
+var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
+
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+
 
 // Deployment to GitHub using git.
-gulp.task('deploy', function (done) {
-  var args = ['push', '-u', 'origin', '<%= prompts.githubBranch %>'];
-  if (process.argv.length > 3) {
-    args.push.apply(args, process.argv.slice(3));
-  }
-  console.log('git', args.join(' '));
+gulp.task('deploy', ['default'], function (done) {
+  runSequence('deploy:commit', 'deploy:push', done);
+});
 
-  var cwd = path.join(process.cwd(), 'dist');
-  spawn('git', args, {cwd: cwd, stdio: 'inherit'}).on('close', function (code) {
-    done();
-    if (code != 0) {
-      process.exit(code);
+// Commit changes to production build locally.
+gulp.task('deploy:commit', function (done) {
+  var msg, im = process.argv.indexOf('-m');
+  if (im >= 0) {
+    msg = process.argv.slice(im, im + 2)[1];
+  }
+  msg = msg || 'Production build';
+  var cmd = ["git add .", "git commit -m '" + msg + "'", "git status"];
+  exec(cmd.join(' && '), {cwd: path.join(process.cwd(), 'dist')}, function (err, stdout, stderr) {
+    if (!/working directory clean/m.test(stdout)) {
+      console.log('--- ERROR log ---\n' + stderr);
+      console.log('--- STDOUT log ---\n' + stdout);
+      throw err || new Error('deploy:commit');
     }
+    done();
+  });
+});
+
+// Push changes to remote (actual deployment).
+gulp.task('deploy:push', function (done) {
+  var cwd = path.join(process.cwd(), 'dist');
+  var args = ['push', '-u', 'origin', '<%= prompts.githubBranch %>'];
+  spawn('git', args, {cwd: cwd, stdio: 'inherit'}).on('close', function (code) {
+    if (code != 0) {
+      throw new Error('deploy:push exit code is ' + code);
+    }
+    done();
   });
 });
